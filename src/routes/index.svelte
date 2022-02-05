4872a871c2df6cc5a7e9a70b16c6480274f6dae2
kingsley <okingsley78@yahoo.com>
2022-02-05 05:19:18 +0000

    app that listen to mouse change and display thire position

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  let mp = {x:0, y:0};
+
+  function handleMousechange(event){
+    mp.x = event.clientX;
+    mp.y = event.clientY;
+  }
+</script>
+
+<style>
+  .plain{
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<div on:mousemove={handleMousechange} class="plain">
+  <h1>The position of the mouse is: {mp.x} , { mp.y }</h1>
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
